72aaeb7b415c7213e1069c09554096b46a8e05a5
Marvin Diaz <md1685268@gmail.com>
2025-08-18 21:22:30 -0700

    Complete Lesson 6 assignment request specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_30_043101) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_14_024937) do
   create_table "coding_classes", force: :cascade do |t|
     t.string "title"
     t.text "description"
